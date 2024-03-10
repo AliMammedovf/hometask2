41111b71ba809b32f949deedc0c9d2908fe3c29b
AliMammedovf <tu7dw5n4h@code.edu.az>
2024-03-10 19:48:56 +0400

    Add project files.

---
+++ hometask2/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace hometask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Verilmis ededin reqemlerinin hasilini tapin.
+
+
+            //int eded;
+            //int qaliq;
+            //int product = 1;
+            //Console.WriteLine("Ededi daxil edin!");
+            //eded = Convert.ToInt32(Console.ReadLine());
+
+            //while (eded != 0)
+            //{
+            //    qaliq = eded % 10;
+            //    eded -= qaliq;
+            //    product = product * qaliq;
+            //    eded = eded / 10;
+
+            //}
+            //Console.WriteLine(product);
+
+            ////Verilmis ededin sade ve ya murekkeb oldugunu tapan alqoritm.
+
+            //int i;
+            //Console.WriteLine("Ededi daxil edin!");
+            //i = Convert.ToInt32(Console.ReadLine());
+            //if (i > 2)
+            //{
+            //    if (i % 2 == 0 || i % 3 == 0)
+            //    {
+
+            //        Console.WriteLine("Eded murekkebdir");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Eded sadedir");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ne sadedir,nede murekkeb");
+            //}
+
+            ////Verilmis ededin faktorialini tapin.
+
+
+            //int eded;
+            //int factorial = 1;
+            //Console.WriteLine("Ededi daxil edin");
+            //eded = Convert.ToInt32(Console.ReadLine());
+
+            //while (eded != 0)
+            //{
+            //    factorial *= eded;
+            //    eded -= 1;
+            //}
+
+            //Console.WriteLine(factorial);
+
+            ////Verilmiş ədədin polindrom olub olmadığını tapan alqoritm(Meselen: 1221->polindrom, 1231->polindrom deyil)(optional)
+
+            //int i;
+            //int qaliq;
+            //int sum = 0;
+            //int temp;
+
+
+            //Console.WriteLine("Ededi daxil edin");
+            //i = Convert.ToInt32(Console.ReadLine());
+            //temp = i;
+
+            //while (i > 0)
+            //{
+            //    qaliq = i % 10;
+            //    sum = (sum * 10) + qaliq;
+            //    i /= 10;
+
+            //}
+            //if (temp == sum)
+            //{
+            //    Console.WriteLine("Eded Polindromdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eded Polindrom deyil");
+            //}
+        }
+    }
+}
